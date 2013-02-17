090d8474c0ced5f75508374f09744af58bded12c
Marc Brinkmann <git@marcbrinkmann.de>
2013-02-17 03:19:42 +0100

    Added zip:// and tar:// sources.

--- gittar/__init__.py
+++ gittar/__init__.py
@@ -7,6 +7,8 @@ import stat
 import time
 from urlparse import urlparse
 import sys
+import tarfile
+from zipfile import ZipFile
 
 from dateutil.tz import tzlocal
 from datetime import datetime
@@ -65,6 +67,10 @@ class Source(object):
 
 def _is_executable(path):
     mode = os.stat(path)[stat.ST_MODE]
+    return _executable_bits(mode)
+
+
+def _executable_bits(mode):
     return mode & stat.S_IXUSR or mode & stat.S_IXGRP or mode & stat.S_IXOTH
 
 
@@ -106,7 +112,42 @@ class FilesystemSource(Source):
             )
 
 
-for source_class in [FilesystemSource]:
+class ZipSource(Source):
+    scheme = 'zip'
+
+    def __iter__(self):
+        with ZipFile(self.url.path) as archive:
+            for name in archive.namelist():
+                with archive.open(name) as f:
+                    yield name, self._def_perm, Blob.from_string(f.read())
+
+
+class TarSource(Source):
+    scheme = 'tar'
+
+    def __iter__(self):
+        with tarfile.open(self.url.path) as archive:
+            for info in archive.getmembers():
+                if info.isdir():
+                    continue
+                elif info.isfile() or info.islnk():
+                    target = info.name if info.isfile()\
+                                       else info.linkname
+                    mode = self._exc_perm if _executable_bits(info.mode)\
+                                          else self._def_perm
+                    buf = archive.extractfile(target).read()
+                elif info.issym():
+                    mode = stat.S_IFLNK
+                    buf = info.linkname
+                else:
+                    raise RuntimeError('Can\'t handle %s in %s' % (
+                        info.name, self.url.geturl()
+                    ))
+
+                yield info.name, mode, Blob.from_string(buf)
+
+
+for source_class in [FilesystemSource, ZipSource, TarSource]:
     SOURCES[source_class.scheme] = source_class
 
 
