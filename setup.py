816f752efe465b06071c37c6a903e167b6e5a6c8
Marc Brinkmann <git@marcbrinkmann.de>
2013-02-18 01:52:00 +0100

    Release 0.2.1.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='gittar',
-    version='0.3dev',
+    version='0.2.1',
     description='The inverse of git archive. Adds a new commit from an archive'
                 'or the filesystem.',
     long_description=read('README.rst'),
